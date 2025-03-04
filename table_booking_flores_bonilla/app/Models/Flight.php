3fe65af4a1b3c9fbf87cc40ad0ae92101e69bd49
DorianIESMM <dflobon1910@g.educaand.es>
2025-03-04 12:44:49 +0100

    Comit Migraciones Modelo

---
+++ table_booking_flores_bonilla/app/Models/Flight.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['airline_id', 'origin_airport_id', 'destination_airport_id', 'departure_time', 'arrival_time', 'price', 'available_seats'];
+
+    public function airline()
+    {
+        return $this->belongsTo(Airline::class);
+    }
+
+    public function origin()
+    {
+        return $this->belongsTo(Airport::class, 'origin_airport_id');
+    }
+
+    public function destination()
+    {
+        return $this->belongsTo(Airport::class, 'destination_airport_id');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+}
+
